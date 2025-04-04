0cca56eb1f1c3cfa9b7fb39f3a5567446bfbe615
vdemers <vdemers@ibm.com>
2025-04-04 12:34:28 -0400

    tt

--- dev-get-lm-desk.sh
+++ dev-get-lm-desk.sh
@@ -584,7 +584,5 @@ fi
 ############################
 if [ "$run_open_webui" == "0" ] && yes_no_prompt "Run Openwebui?"
 then
-    run $ollama_bin start || 
-    echo $uv_bin ||
-    run $uv_bin run https://raw.githubusercontent.com/vedem1192/lm-desk/refs/heads/main/scripts/openwebui.py
+    run $ollama_bin start || run $uv_bin run https://raw.githubusercontent.com/vedem1192/lm-desk/refs/heads/main/scripts/openwebui.py
 fi
