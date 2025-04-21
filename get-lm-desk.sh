a79cfe0ba4ca6d3df854ceeabe4847477990037a
Gabe Goodhart <ghart@us.ibm.com>
2025-04-21 10:12:01 -0600

    fix: Require brew install if installing beeai
    Branch: NeedBrewForBee
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- get-lm-desk.sh
+++ get-lm-desk.sh
@@ -679,12 +679,9 @@ fi
 # Install brew if needed to install other tools #
 #################################################
 need_brew="0"
-if [ "$ollama_bin" == "" ]
+if [ "$ollama_bin" == "" ] || [ "$beeai_bin" == "" ]
 then
-    if [ "$brew_bin" == "" ] && [ "$curl_bin" == "" ]
-    then
-        need_brew="1"
-    fi
+    need_brew="1"
 fi
 if [ "$need_brew" == "1" ] && yes_no_prompt "Install brew?"
 then
